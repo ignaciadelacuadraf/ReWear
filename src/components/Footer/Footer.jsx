db805b2378e16cfab15f35f585fe4709baa87680
MARIA IGNACIA DE LA CUADRA <ignaciadelacuadraf@uc.cl>
2025-10-23 23:54:02 -0300

    About US

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
+// src/components/Footer/Footer.jsx (o donde lo tengas)
+
 import "./Footer.css";
-import { Link } from "react-router-dom"; // <-- LÍNEA NUEVA (importar Link)
+import { Link } from "react-router-dom"; 
 
 export default function Footer() {
   return (
@@ -12,13 +14,7 @@ export default function Footer() {
             <li>mail: ventas@rewear.com</li>
             <li>instagram: @rewear_</li>
             <li>celular: +56 9 7374 2974</li>
-            
-            {/* --- AQUÍ ESTÁ EL CAMBIO --- */}
-            <li>
-              <Link to="/nosotras">Sobre Nosotras</Link>
-            </li>
-            {/* ---------------------------- */}
-
+            {/* El link de "Sobre Nosotras" se ha movido */}
           </ul>
         </div>
 
@@ -47,15 +43,22 @@ export default function Footer() {
         <div className="footer__col">
           <h4 className="footer__title">Políticas</h4>
           <ul className="footer__list">
-            {/*
-              NOTA: He cambiado los <a href> por <Link to> 
-              para que la navegación sea interna de React y no recargue la página. 
-              Asegúrate de que estas rutas existan en tu router.jsx
-            */}
             <li><Link to="/policies/refund">Política de reembolso</Link></li>
             <li><Link to="/policies/shipping">Política de envío</Link></li>
           </ul>
         </div>
+
+        {/* ===== COLUMNA 4 (NUEVA) ===== */}
+        <div className="footer__col">
+          <h4 className="footer__title">Sobre Nosotras</h4>
+          <ul className="footer__list">
+            <li>
+              <Link to="/nosotras">Conoce al equipo</Link>
+            </li>
+          </ul>
+        </div>
+        {/* =============================== */}
+
       </div>
 
       <div className="footer__bottom">
