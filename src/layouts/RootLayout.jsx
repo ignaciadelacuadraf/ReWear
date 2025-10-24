3208dfbfe578c31a5f83d6c6b1262cef7c2cd271
MARIA IGNACIA DE LA CUADRA <ignaciadelacuadraf@uc.cl>
2025-10-23 23:59:26 -0300

    ABout US y autoscrolleo para arriba

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,18 @@
+// src/layouts/RootLayout.jsx
+
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
+// --- 1. IMPORTA EL NUEVO COMPONENTE ---
+// (Asegúrate que la ruta a tu archivo sea correcta. 
+// Te recomiendo crearlo en 'src/utils/ScrollToTop.jsx')
+import ScrollToTop from '../utils/ScrollToTop';
+
 export default function RootLayout() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Outlet />
